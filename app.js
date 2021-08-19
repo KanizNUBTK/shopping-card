49f6dac245c9b41ec5e9438060feea71c95e57fc
KanizNUBTK <kaniz.m.mim@gmail.com>
2021-08-19 09:26:08 +0600

    all work finish

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 console.log('okey');
-function updateCaseNumber(isIncreasing, inputId) {
-    const caseInput = document.getElementById(inputId);
+function updateCaseNumber(product, price, isIncreasing) {
+    const caseInput = document.getElementById(product + '-number');
     let caseNumber = caseInput.value;
     if (isIncreasing == true) {
         caseNumber = parseInt(caseNumber) + 1;
@@ -9,20 +9,36 @@ function updateCaseNumber(isIncreasing, inputId) {
         caseNumber = parseInt(caseNumber) - 1;
     }
     caseInput.value = caseNumber;
-    const caseTotal = document.getElementById('case-total');
-    caseTotal.innerText = caseNumber * 59;
+    const caseTotal = document.getElementById(product + '-total');
+    caseTotal.innerText = caseNumber * price;
+    calculateTotal();
+}
+function getInputValue(product) {
+    const phoneInput = document.getElementById(product + '-number');
+    const phoneNumber = parseInt(phoneInput.value);
+    return phoneNumber;
+}
+function calculateTotal() {
+    const phoneTotal = getInputValue('phone') * 1219;
+    const caseTotal = getInputValue('case') * 59;
+    const subTotal = phoneTotal + caseTotal;
+    const tax = subTotal / 10;
+    const totalPrice = subTotal + tax;
+    document.getElementById('sub-total').innerText = subTotal;
+    document.getElementById('tax-amount').innerText = tax;
+    document.getElementById('total-price').innerText = totalPrice;
 }
 // product1
 document.getElementById('case-plus').addEventListener('click', function () {
-    updateCaseNumber(true, 'case-number');
+    updateCaseNumber('case', 59, true);
 })
 document.getElementById('case-minus').addEventListener('click', function () {
-    updateCaseNumber(false, 'case-number');
+    updateCaseNumber('case', 59, false);
 })
 //  product2
 document.getElementById('phone-plus').addEventListener('click', function () {
-    updateCaseNumber(true, 'phone-number');
+    updateCaseNumber('phone', 1219, true);
 })
 document.getElementById('phone-minus').addEventListener('click', function () {
-    updateCaseNumber(true, 'phone-number');
+    updateCaseNumber('phone', 1219, false);
 })
